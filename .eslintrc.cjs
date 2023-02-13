1fe87780797c9e7d9f4777df5b1507a7fd8e14c3
Ryan Tsai <a0986575797@gmail.com>
2023-02-13 15:16:05 +0800

    feat:all files

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'airbnb-base',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue'],
+      },
+    },
+    'import/core-modules': [
+      'vite',
+      '@vitejs/plugin-vue',
+      '@rushstack/eslint-patch/modern-module-resolution',
+    ],
+  },
+};
